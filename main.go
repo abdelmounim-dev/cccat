035e01c4bdb845218749f92958b1141edd40640e
Abdelmounim Baouia <abdelmounim.b@edraak.io>
2024-07-14 13:45:24 +0100

    feature: print numbered lines, print numberd non-blank lines

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
+	"io"
 	"os"
 )
 
@@ -17,20 +18,95 @@ func fileExists(filename string) bool {
 	return false
 }
 
+func isFlag(arg string) bool {
+	return len(arg) > 0 && arg[0] == '-'
+}
+
+func getFlagsAndFiles(args []string) ([]string, []string) {
+	flags := []string{}
+	files := []string{}
+	for _, arg := range args {
+		if isFlag(arg) {
+			flags = append(flags, arg)
+		} else {
+			files = append(files, arg)
+		}
+	}
+	return flags, files
+}
+
+func numberType(flags []string) rune {
+	for _, flag := range flags {
+		if flag == "-n" {
+			return 'n'
+		}
+		if flag == "-b" {
+			return 'b'
+		}
+	}
+	return 'g'
+}
+
+func printNormal(scanner bufio.Scanner) {
+	for scanner.Scan() {
+		text := scanner.Text()
+		fmt.Println(text)
+	}
+}
+
+func printNumbered(scanner bufio.Scanner) {
+	n := 1
+	for scanner.Scan() {
+		text := scanner.Text()
+		fmt.Println(n, " ", text)
+		n++
+	}
+}
+
+func printNumberedNonBlank(scanner bufio.Scanner) {
+	n := 1
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text != "" {
+			fmt.Println(n, " ", text)
+			n++
+		} else {
+			fmt.Println(text)
+		}
+	}
+}
+
+func readFromReader(r io.Reader, flags []string) {
+	scanner := bufio.NewScanner(r)
+
+	nt := numberType(flags)
+	if nt == 'n' {
+		printNumbered(*scanner)
+	} else if nt == 'b' {
+		printNumberedNonBlank(*scanner)
+	} else {
+		printNormal(*scanner)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Println("not implemented yet")
+	flags, files := getFlagsAndFiles(os.Args[1:])
+	if len(files) == 0 {
+		readFromReader(os.Stdin, flags)
 		return
 	}
-	for i := 1; i < len(os.Args); i++ {
-		fileName := os.Args[i]
-		if !fileExists(fileName) {
-			log.Fatalf("file %s doesn't exist", fileName)
-		}
-		content, err := os.ReadFile(fileName)
+
+	for i := 0; i < len(files); i++ {
+		file, err := os.Open(files[i])
 		if err != nil {
-			log.Fatal("error reading file: ", err)
+			fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+			return
 		}
-		fmt.Printf("%s\n", string(content))
+		defer file.Close()
+		readFromReader(file, flags)
 	}
 }
